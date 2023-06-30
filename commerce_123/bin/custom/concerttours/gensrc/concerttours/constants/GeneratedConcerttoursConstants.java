0f3ad5296bf921f67c8b1e47b057ecf2ff9d082a
Roger Ramirez <roger8849@gmail.com>
2023-06-29 21:18:02 -0500

    Add commerce 123 source code.

---
+++ commerce_123/bin/custom/concerttours/gensrc/concerttours/constants/GeneratedConcerttoursConstants.java
@@ -0,0 +1,62 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at Nov 9, 2022, 7:38:44 PM                     ---
+ * ----------------------------------------------------------------
+ */
+package concerttours.constants;
+
+/**
+ * @deprecated since ages - use constants in Model classes instead
+ */
+@Deprecated
+@SuppressWarnings({"unused","cast"})
+public class GeneratedConcerttoursConstants
+{
+	public static final String EXTENSIONNAME = "concerttours";
+	public static class TC
+	{
+		public static final String BAND = "Band".intern();
+		public static final String CONCERT = "Concert".intern();
+		public static final String CONCERTTYPE = "ConcertType".intern();
+		public static final String MUSICTYPE = "MusicType".intern();
+		public static final String NEWS = "News".intern();
+		public static final String NOTLOREMIPSUMCONSTRAINT = "NotLoremIpsumConstraint".intern();
+	}
+	public static class Attributes
+	{
+		public static class MusicType
+		{
+			public static final String BANDS = "bands".intern();
+		}
+		public static class Product
+		{
+			public static final String BAND = "band".intern();
+			public static final String HASHTAG = "hashtag".intern();
+		}
+	}
+	public static class Enumerations
+	{
+		public static class ConcertType
+		{
+			public static final String OPENAIR = "openair".intern();
+			public static final String INDOOR = "indoor".intern();
+		}
+		public static class MusicType
+		{
+			// no values defined
+		}
+	}
+	public static class Relations
+	{
+		public static final String BAND2MUSICTYPE = "Band2MusicType".intern();
+		public static final String PRODUCT2ROCKBAND = "Product2RockBand".intern();
+	}
+	
+	protected GeneratedConcerttoursConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
---
+++ commerce_123/bin/custom/concerttours/src/concerttours/service/NewsService.java
@@ -0,0 +1,10 @@
+package concerttours.service;
+
+import java.util.Date;
+import java.util.List;
+import concerttours.model.NewsModel;
+
+public interface NewsService {
+
+  List<NewsModel> getNewsOfTheDay(Date date);
+}
